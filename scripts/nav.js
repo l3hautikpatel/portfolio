8c0de07ab54e8ca1b10076bec95552be57c9b75a
l3hautikpatel <bhautikp105@gmail.com>
2024-08-06 21:27:15 -0400

    navbar fixed and working all fine all the othe part are disable for devloped purpose

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,33 +1,26 @@
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links');
+const backdrop = document.querySelector('.menu-backdrop');
 
 burger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    burger.classList.toggle('toggle');
+  navLinks.classList.toggle('nav-active');
+  backdrop.classList.toggle('display');
+  burger.classList.toggle('toggle');
+  document.body.classList.toggle('fixed-position');
 });
 
-
-
-const body = document.querySelector('body');
-const backdrop = document.createElement('div');
-backdrop.classList.add('menu-backdrop');
-
-body.appendChild(backdrop);
-
-burger.addEventListener('click', () => {
-    navLinks.classList.toggle('nav-active');
-    backdrop.classList.toggle('display'); // Show or hide the backdrop
-
-    // Toggle body scrolling
-    body.classList.toggle('fixed-position');
-
-    // Burger Animation
-    burger.classList.toggle('toggle');
+backdrop.addEventListener('click', () => {
+  navLinks.classList.remove('nav-active');
+  backdrop.classList.remove('display');
+  burger.classList.remove('toggle');
+  document.body.classList.remove('fixed-position');
 });
 
-backdrop.addEventListener('click', function () {
+navLinks.addEventListener('click', (e) => {
+  if (e.target.tagName === 'A') {
     navLinks.classList.remove('nav-active');
-    this.classList.remove('display'); // Hide the backdrop when clicked
-    body.classList.remove('fixed-position');
+    backdrop.classList.remove('display');
     burger.classList.remove('toggle');
+    document.body.classList.remove('fixed-position');
+  }
 });
